a90a2e78e283a905539ca79fb5d1d87e60ccfbab
JordanHeVe <jordan.herreravera@gmail.com>
2023-12-12 11:58:02 -0600

    added pasword generator code

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -7,18 +7,48 @@ const checkBoxes = document.querySelectorAll('input[type=checkbox]');
 let range = document.querySelector('.range');
 let lenghtNumber = document.querySelector('.lenght-number');
 let barsLvl = document.querySelector('#bars');
+let checkUppercase = document.querySelector('#uppercase');
+let checkLowercase = document.querySelector('#lowercase');
+let checkNumbers = document.querySelector('#numbers');
+let checkSymbols = document.querySelector('#symbols');
+window.onload = () => {
+    range.value = '10';
+    checkBoxes.forEach(checkbox => checkbox.checked = false);
+};
 const checkboxChars = {
     uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
     lowercase: 'abcdefghijklmnopqrstuvwxyz',
     numbers: "0123456789",
-    symbols: "!@#$%^&*()_+~`|}{[]\\:;?><,./-="
+    symbols: "!@#$%^&*()"
 };
-console.log(checkBoxes);
 const generatePassword = (e) => {
     e.preventDefault();
     let currentRangeValue = parseInt(range.value);
-    if (levelText) {
-        levelText.innerHTML = calculateSecurityLevel(currentRangeValue, checkBoxes);
+    let chars = '';
+    let passwordGenerated = '';
+    const checkedCount = Array.from(checkBoxes).filter(checkbox => checkbox.checked).length;
+    if (levelText && checkedCount > 0) {
+        levelText.innerHTML = calculateSecurityLevel(currentRangeValue, checkedCount);
+    }
+    if (checkUppercase.checked) {
+        chars += checkboxChars.uppercase;
+    }
+    if (checkLowercase.checked) {
+        chars += checkboxChars.lowercase;
+    }
+    if (checkNumbers.checked) {
+        chars += checkboxChars.numbers;
+    }
+    if (checkSymbols.checked) {
+        chars += checkboxChars.symbols;
+    }
+    if (chars.length > 0 && password) {
+        for (let i = 0; i <= currentRangeValue; i++) {
+            let randomNumber = Math.floor(Math.random() * chars.length);
+            passwordGenerated += chars.substring(randomNumber, randomNumber + 1);
+        }
+        password.innerHTML = passwordGenerated;
+        password.classList.remove('placeholder');
     }
 };
 const updateTxt = (e) => {
@@ -27,23 +57,20 @@ const updateTxt = (e) => {
         lenghtNumber.innerHTML = currentRangeValue;
     }
 };
-const calculateSecurityLevel = (length, selectedCheckboxes) => {
-    const checkedCount = Array.from(selectedCheckboxes).filter(checkbox => checkbox.checked).length;
-    console.log(length);
-    console.log(checkedCount);
+const calculateSecurityLevel = (length, checkedCount) => {
     if (length < 8 || checkedCount === 1) {
         barsLvl === null || barsLvl === void 0 ? void 0 : barsLvl.setAttribute('class', 'too-weak');
         return 'Too Weak!';
     }
-    else if (length < 12 && checkedCount > 1) {
+    else if (length < 13 && checkedCount < 3) {
         barsLvl === null || barsLvl === void 0 ? void 0 : barsLvl.setAttribute('class', 'weak');
         return 'Weak';
     }
-    else if (length <= 20 && checkedCount >= 2) {
+    else if (length < 16 && checkedCount <= 4) {
         barsLvl === null || barsLvl === void 0 ? void 0 : barsLvl.setAttribute('class', 'medium');
         return 'Medium';
     }
-    else if (length >= 16 && checkedCount >= 3) {
+    else if (length >= 16 && checkedCount > 3) {
         barsLvl === null || barsLvl === void 0 ? void 0 : barsLvl.setAttribute('class', 'strong');
         return 'Strong';
     }
@@ -52,4 +79,4 @@ const calculateSecurityLevel = (length, selectedCheckboxes) => {
     }
 };
 form === null || form === void 0 ? void 0 : form.addEventListener('submit', generatePassword);
-range === null || range === void 0 ? void 0 : range.addEventListener('change', updateTxt);
+range === null || range === void 0 ? void 0 : range.addEventListener('input', updateTxt);
--- scripts/ts/main.ts
+++ scripts/ts/main.ts
@@ -1,4 +1,4 @@
-let password = document.querySelector('.password-txt')
+let password = document.querySelector('.password-txt') as HTMLSpanElement
 let form = document.querySelector('.generator-form')
 let copyButton =  document.querySelector('copy-btn')
 let levelText = document.querySelector('.current-lvl')
@@ -6,23 +6,60 @@ const checkBoxes = document.querySelectorAll<HTMLInputElement>('input[type=check
 let range = document.querySelector('.range') as HTMLInputElement
 let lenghtNumber = document.querySelector('.lenght-number')
 let barsLvl = document.querySelector('#bars')
-const checkboxChars:object = {
+let checkUppercase = document.querySelector('#uppercase') as HTMLInputElement
+let checkLowercase = document.querySelector('#lowercase') as HTMLInputElement
+let checkNumbers = document.querySelector('#numbers') as HTMLInputElement
+let checkSymbols = document.querySelector('#symbols') as HTMLInputElement
+
+window.onload = () => {
+
+    range.value = '10';
+
+    checkBoxes.forEach(checkbox => checkbox.checked = false);
+}
+
+const checkboxChars = {
     uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
     lowercase: 'abcdefghijklmnopqrstuvwxyz',
     numbers: "0123456789",
-    symbols: "!@#$%^&*()_+~`|}{[]\\:;?><,./-="
+    symbols: "!@#$%^&*()"
 }
 
-console.log(checkBoxes)
-
-const generatePassword = (e:Event) => {
+const generatePassword = (e:Event):void => {
     e.preventDefault();
     let currentRangeValue: number = parseInt(range.value)
-    if(levelText){
-        levelText.innerHTML= calculateSecurityLevel(currentRangeValue, checkBoxes )
+    let chars = ''
+    let passwordGenerated = ''
+    const checkedCount = Array.from(checkBoxes).filter(checkbox => checkbox.checked).length;
+    if(levelText &&   checkedCount > 0){
+        levelText.innerHTML= calculateSecurityLevel(currentRangeValue, checkedCount )
+    }
+
+    if(checkUppercase.checked){
+        chars+=checkboxChars.uppercase
+    }
+    if(checkLowercase.checked){
+        chars+=checkboxChars.lowercase
+    }
+    if(checkNumbers.checked){
+        chars+=checkboxChars.numbers
+    }
+    if(checkSymbols.checked){
+        chars+=checkboxChars.symbols
+    }
+
+    if(chars.length > 0 && password) {
+        for(let i=0; i <= currentRangeValue ; i++) {
+             let randomNumber = Math.floor(Math.random()* chars.length)
+             passwordGenerated += chars.substring(randomNumber, randomNumber +1);
+        }
+        password.innerHTML = passwordGenerated
+        password.classList.remove('placeholder')
+        
     }
 
 }
+
 
 const updateTxt = (e:Event) => {
     let currentRangeValue: string = (e.target as HTMLInputElement).value;
@@ -32,20 +69,18 @@ const updateTxt = (e:Event) => {
 
 }
 
-const calculateSecurityLevel = (length: number, selectedCheckboxes: NodeListOf<HTMLInputElement>): string => {
-    const checkedCount = Array.from(selectedCheckboxes).filter(checkbox => checkbox.checked).length;
-     console.log(length)
-    console.log(checkedCount)
-    if (length < 8 || checkedCount === 1) {
+const calculateSecurityLevel = (length: number, checkedCount:number): string => {
+
+    if ( length < 8 || checkedCount === 1) {
         barsLvl?.setAttribute('class', 'too-weak')
         return 'Too Weak!';
-    } else if (length < 12 && checkedCount > 1) {
+    } else if (length < 13 && checkedCount < 3) {
         barsLvl?.setAttribute('class', 'weak')
         return 'Weak';
-    } else if (length <= 20 && checkedCount >= 2) {
+    } else if (length < 16 && checkedCount <= 4 ) {
         barsLvl?.setAttribute('class', 'medium')
         return 'Medium';
-    } else if (length >= 16 && checkedCount >= 3) {
+    } else if (length >= 16 && checkedCount > 3) {
         barsLvl?.setAttribute('class', 'strong')
         return 'Strong';
     } else {
@@ -54,5 +89,6 @@ const calculateSecurityLevel = (length: number, selectedCheckboxes: NodeListOf<H
 };
 
 
+
 form?.addEventListener('submit', generatePassword)
-range?.addEventListener('change', updateTxt)
+range?.addEventListener('input', updateTxt)
